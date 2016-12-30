1026e8ccb75659b0190504fa76ebc5d43fa01994
John Hunter <john@push-it-forward.com>
2016-12-29 19:09:06 -0800

    initial commit

---
+++ bs_util.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def getBSObj(url):
+
+	try:
+		html = urlopen(url)
+	except HTTPError as e:
+		print(e)
+		return None
+	except URLError as e:
+		print(e)
+		return None
+	
+	try:
+		bsObj = BeautifulSoup(html.read(), "html.parser")
+	except AttributeError as e:
+		print(e)
+		return None
+
+	return bsObj
