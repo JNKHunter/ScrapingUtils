7720e0cc6c92efa4d4d3e7b73b24e8cbfec3a772
John Hunter <john@push-it-forward.com>
2016-12-30 17:29:47 -0800

    added site crawler

---
+++ crawl_site.py
@@ -0,0 +1,19 @@
+from bs_util import getBSObj
+import re
+
+pages = set()
+
+def getLinks(pageUrl):
+	global pages
+	bsObj = getBSObj(pageUrl)
+	for link in bsObj.findAll("a",href=re.compile("[^http:]")):
+		if 'href' in link.attrs:
+			if link.attrs['href'] not in pages:
+				newPage = link.attrs['href']
+				print(newPage)
+				pages.add(newPage)
+				getLinks(newPage)
+
+
+
+
